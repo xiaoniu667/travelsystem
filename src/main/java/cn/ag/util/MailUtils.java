a9ebd2e5b4942ba19f54a7a13b098f596c7ef6e0
xiaoniu665 <578812414@qq.com>
2022-01-22 11:22:03 +0800

    666

--- src/main/java/cn/ag/util/MailUtils.java
+++ src/main/java/cn/ag/util/MailUtils.java
@@ -9,7 +9,7 @@ import java.util.Properties;
  * 发邮件工具类
  */
 public final class MailUtils {
-    private static final String USER = "2486483837@qq.com"; // 发件人称号，同邮箱地址
+    private static final String USER = "12486483837@qq.com"; // 发件人称号，同邮箱地址
     private static final String PASSWORD = "hhhfypseztrdeaid"; // 如果是qq邮箱可以使户端授权码，或者登录密码
 
     /**
@@ -68,7 +68,7 @@ public final class MailUtils {
     }
 
     public static void main(String[] args) throws Exception { // 做测试用
-        MailUtils.sendMail("2486483837@qq.com","你好，这是一封测试邮件，无需回复。","测试邮件");
+        MailUtils.sendMail("12486483837@qq.com","你好，这是一封测试邮件，无需回复。","测试邮件");
         //邮件需要改一下
         System.out.println("发送成功");
     }
